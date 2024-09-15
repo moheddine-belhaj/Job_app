709e9e573f459139e96f5dc0c1c7fe8f39af401c
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-09-15 19:47:19 +0200

    testing

--- job_app/src/App.vue
+++ job_app/src/App.vue
@@ -1,4 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+
+
+    const name = ref( 'JOB JOB');
+    const status = ref( 'active');
+    const tasks = ref(['task1','task2','task3']);
+    const newTask = ref('');
+
+    const changeStatus = () => {
+
+      if (status.value === 'active') {
+        status.value = 'pending';
+      } else if (status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+  };
+
+  const addTask = () => {
+    tasks.value.push(newTask.value);
+    newTask.value = '' ;
+  };
+
+  const DeleteTask = (index) => {
+    tasks.value.splice(index , 1000000);
+  };
+</script>
+
 <template>
-<h1>JOBS</h1>
+<h1>{{ name}}</h1>
+<p v-if="status === 'active'">User is active</p>
+<p v-else-if="status === 'pending'">User is pending</p>
+<p v-else="status">User is inactive</p>
+<h3>Tasks</h3>
+<ul>
+  <li v-for="(task , index) in tasks" :key="task">
+    <span>
+      {{ task }} 
+    </span>
+    <button @click="DeleteTask()"> X </button>
+  </li>
+</ul>
+<button @click="changeStatus"  >
+change
+</button>
+
+<form @submit.prevent="addTask">
+<label for="newTask"> Add Task</label>
+<input type="text" id="newTask" name="newTask" v-model="newTask">
+<button type="submit">Submit</button>
+</form>
 </template>
 
---
+++ job_app/src/App2.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data () {
+    return {
+      name:'JOB JOB',
+      status: 'active',
+      tasks :['task1','task2','task3'],
+      link:'https://www.google.com/',
+
+    };
+  },
+
+  methods: {
+
+      changeStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+      }
+  }
+  };
+
+
+</script>
+
+<template>
+<h1>{{ name}}</h1>
+<p v-if="status === 'active'">User is active</p>
+<p v-else-if="status === 'pending'">User is pending</p>
+<p v-else="status">User is inactive</p>
+<h3>Tasks</h3>
+<ul>
+  <li v-for="task in tasks" :key="task"> {{ task }} </li>
+</ul>
+<a :href="link"> Go to google</a>
+<button @click="changeStatus"  >
+change
+</button>
+</template>
+
